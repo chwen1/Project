d884b5d997578ffc2e03ea277a25fd8dc17493e7
april <ch9540@sina.com>
2020-01-07 17:20:28 +0800

    Add HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    int i = 10;
+    int j =5;
+    int a = i + j;
+    int b = i - j;
+    int c = i*j;
+    int d = i /j;
+    }
+}
